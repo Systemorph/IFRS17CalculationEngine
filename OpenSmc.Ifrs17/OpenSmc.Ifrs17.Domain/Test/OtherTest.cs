4085f84d41bb460d8b6347b42bdbfd4a224d1fff
Andrey Katz <akatz@systemorph.com>
2024-01-24 18:11:45 +0100

    Other tests

--- OpenSmc.Ifrs17/OpenSmc.Ifrs17.Domain/Test/OtherTest.cs
+++ OpenSmc.Ifrs17/OpenSmc.Ifrs17.Domain/Test/OtherTest.cs
@@ -1,35 +1,86 @@
-#!eval-notebook "../Utils/ImportCalculationMethods"
+using FluentAssertions;
+using OpenSmc.Ifrs17.Domain.Utils;
+
+//#!eval-notebook "../Utils/ImportCalculationMethods"
 
 
-Enumerable.Repeat(1e6/12, 12)
-    .NewBusinessInterestAccretion(new[]{1.00407412378365}, 12, 0)
-    .Should().BeApproximately(26881.4607, 0.001);
+public class OtherTests
+{
+    public void TestsCollection()
+    {
+        Enumerable.Repeat(1e6 / 12, 12)
+            .NewBusinessInterestAccretion(new[]
+            {
+                1.00407412378365
+            }, 12, 0)
+            .Should().BeApproximately(26881.4607, 0.001);
 
 
-Enumerable.Repeat(1e6/12, 12)
-    .NewBusinessInterestAccretion(new[]{1.0}, 12, 0)
-    .Should().BeApproximately(0, 0.001);
+        Enumerable.Repeat(1e6 / 12, 12)
+            .NewBusinessInterestAccretion(new[]
+            {
+                1.0
+            }, 12, 0)
+            .Should().BeApproximately(0, 0.001);
 
 
-#!import "../Utils/Extensions"
+//#!import "../Utils/Extensions"
 
 
-new [] {0.5,0.4,0.3,0.2,0.1}.Prune(1e-5).SequenceEqual(new [] {0.5,0.4,0.3,0.2,0.1}).Should().BeTrue();
+        new[]
+        {
+            0.5, 0.4, 0.3, 0.2, 0.1
+        }.Prune(1e-5).SequenceEqual(new[]
+        {
+            0.5, 0.4, 0.3, 0.2, 0.1
+        }).Should().BeTrue();
 
 
-new [] {0.5,0.4,0.3,0.2,0.1}.Prune(0.2).SequenceEqual(new [] {0.5,0.4,0.3,0.2}).Should().BeTrue();
+        new[]
+        {
+            0.5, 0.4, 0.3, 0.2, 0.1
+        }.Prune(0.2).SequenceEqual(new[]
+        {
+            0.5, 0.4, 0.3, 0.2
+        }).Should().BeTrue();
 
 
-new [] {0.5,0.4,0.3,0.2,0.1}.Prune(0.3).SequenceEqual(new [] {0.5,0.4,0.3}).Should().BeTrue();
+        new[]
+        {
+            0.5, 0.4, 0.3, 0.2, 0.1
+        }.Prune(0.3).SequenceEqual(new[]
+        {
+            0.5, 0.4, 0.3
+        }).Should().BeTrue();
 
 
-new [] {0.5,0.4,0.3,0.2,0.1}.PruneButFirst(0.3).SequenceEqual(new [] {0.5,0.4,0.3,0.0}).Should().BeTrue();
+        new[]
+        {
+            0.5, 0.4, 0.3, 0.2, 0.1
+        }.PruneButFirst(0.3).SequenceEqual(new[]
+        {
+            0.5, 0.4, 0.3, 0.0
+        }).Should().BeTrue();
 
 
-new [] {0.5,0.2,0.0,0.0,0.0}.PruneButFirst(0.3).SequenceEqual(new [] {0.5,0.0}).Should().BeTrue();
+        new[]
+        {
+            0.5, 0.2, 0.0, 0.0, 0.0
+        }.PruneButFirst(0.3).SequenceEqual(new[]
+        {
+            0.5, 0.0
+        }).Should().BeTrue();
 
 
-new [] {0.5,0.0,0.0,0.0,0.0}.PruneButFirst(0.3).SequenceEqual(new [] {0.5,0.0}).Should().BeTrue();
+        new[]
+        {
+            0.5, 0.0, 0.0, 0.0, 0.0
+        }.PruneButFirst(0.3).SequenceEqual(new[]
+        {
+            0.5, 0.0
+        }).Should().BeTrue();
+    }
+}
 
 
 
