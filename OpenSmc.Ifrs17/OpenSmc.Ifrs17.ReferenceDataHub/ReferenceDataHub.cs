2bf3b57356332fc1f0b5913339cd62d93b77774e
amuolo <and.muolo@gmail.com>
2024-01-31 17:30:00 +0100

    ref data hub init

---
+++ OpenSmc.Ifrs17/OpenSmc.Ifrs17.ReferenceDataHub/ReferenceDataHub.cs
@@ -0,0 +1,16 @@
+﻿using OpenSmc.Activities;
+using OpenSmc.Import.Builders;
+using OpenSmc.Messaging;
+using OpenSmc.ServiceProvider;
+
+namespace OpenSmc.Ifrs17.ReferenceDataHub;
+
+public class ReferenceDataHub : MessageHubPlugin<ImportPlugin>
+{
+    [Inject] private IActivityService activityService;
+
+    public ReferenceDataHub(IMessageHub hub, MessageHubConfiguration options) : base(hub)
+    {
+        options = options.AddImport(x => x);
+    }
+}
