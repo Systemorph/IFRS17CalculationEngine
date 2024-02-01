1c022271e2e09de2a6a1ed3eb404dca1f535673b
amuolo <and.muolo@gmail.com>
2024-02-01 17:28:56 +0100

    finalizing the DataPlugin with Andrey Danilo

--- OpenSmc.Ifrs17/OpenSmc.Ifrs17.ReferenceDataHub/DataPlugin.cs
+++ OpenSmc.Ifrs17/OpenSmc.Ifrs17.ReferenceDataHub/DataPlugin.cs
@@ -7,22 +7,25 @@ using OpenSmc.Ifrs17.Domain.DataModel.KeyedDimensions;
 using OpenSmc.Messaging;
 using OpenSmc.Reflection;
 using OpenSmc.ServiceProvider;
-using OpenSmc.Workspace;
 using System.Collections.Immutable;
-using System.Reflection;
+using IWorkspace = OpenSmc.Workspace.IWorkspace;
 
 namespace OpenSmc.Ifrs17.ReferenceDataHub;
 
-/* For now, this hub owns dimensions solely (subject to change).
- * This hub performs CRUD operations, e.g.
- *  - create: either instantiate the state from DB or perform an in-memory init with import
- *  - read: respond with one or more collection of dimension instances
- *  - update: after a successful import, this hub receives an update request with the new data
- *  
- *  This hub needs to be attached to the ImportPlugin because in case of a in-memory init,
- *  template dimensions have to be written from files.
+/* AM (1.2.2024) todo list:
+ *  a) organize better the IFRS17 project structure
+ *  b) orchestrate which data go to which hub, e.g. refDataHub owns all dimensions
+ *      write the configuration for parameters, transactionalData, etc etc
+ *  c) finish setting up all model hubs simply by means of this generic DataPlugin
+ *  d) implement tests for the DataPlugin by adding tests in the OpenSMC repo
+ *  e) test the IFRS17 model hubs writing the level above this configurations
+ *      to do this check the existing tests in the OpenSMC, e.g. MessageHubTest
+ *      then write the configuration to define routing, addresses, forwarding, etc $
+ *  f) think at the viewModelHub, what to do here? where to start?
+ *      look at the existing tests in OpenSMC, e.g. LayoutTest
+ *  g) monitor the development of the import/export plugin so that we can use them here
+ *      in smc v1 
  */
-
 public static class IfrsConfiguration
 {
     public static MessageHubConfiguration ConfigurationReferenceDataHub(this MessageHubConfiguration configuration)
@@ -31,10 +34,15 @@ public static class IfrsConfiguration
         var dataSource = configuration.ServiceProvider.GetService<IDataSource>();
 
         return configuration
-            .AddData(data => data.WithType<Scenario>( /* Extend With Type to avoid monkey code*/ 
-                async () => await dataSource.Query<Scenario>().ToArrayAsync(),
-                (scenarios) => dataSource.UpdateAsync(scenarios),
-                (scenarios) => dataSource.DeleteAsync(scenarios)));
+            .AddData(data => data
+                .WithType<Scenario>( // TODO: Extend With Type to avoid monkey code
+                    async () => await dataSource.Query<Scenario>().ToArrayAsync(),
+                    (scenarios) => dataSource.UpdateAsync(scenarios),
+                    (scenarios) => dataSource.DeleteAsync(scenarios))
+                .WithType<AmountType>(
+                    async () => await dataSource.Query<AmountType>().ToArrayAsync(),
+                    (amountTypes) => dataSource.UpdateAsync(amountTypes),
+                    (amountTypes) => dataSource.DeleteAsync(amountTypes)));
     }
 
     public static MessageHubConfiguration ConfigurationTransactionalDataHub(this MessageHubConfiguration configuration)
@@ -46,7 +54,7 @@ public static class IfrsConfiguration
             .AddData(data => data.WithType<RawVariable>(
                 async () => await dataSource.Query<RawVariable>().ToArrayAsync(),
                 (scenarios) => dataSource.UpdateAsync(scenarios),
-                (scenarios) => dataSource.DeleteAsync(scenarios))); /* This is delete of data, not of the hub*/
+                (scenarios) => dataSource.DeleteAsync(scenarios))); /* This is delete of data, not of the hub */
         /* Delete of Hub must be implemented separately (pr)*/
     }
 
@@ -55,6 +63,17 @@ public static class IfrsConfiguration
         // TODO: this needs to be registered in the higher level
         //var dataSource = configuration.ServiceProvider.GetService<IDataSource>();
         // TODO: What content should be here? -A.K.
+
+        /* AM (1.2.2024) personal idea:
+         *  The view model (VM) hub is attached to the browser tab, namely there is one per tab.
+         *  When one control (e.g. import button) hosted in this VM signals that an import has to start,
+         *  an import request should be sent to the ImportPlugin. Hence the ImportPlugin has to be 
+         *  'attached' to this VM. 
+         *  
+         *  This also grants scalability. Because ImportPlugin knows the import formats and calls 
+         *  the method to perform the calculation, this setup ensures one calculation can take place
+         *  per browser tab.
+         */
 
         return configuration;
     }
@@ -90,12 +109,14 @@ public record TypeConfiguration<T> (
     Func<IReadOnlyCollection<T>, Task> Save,
     Func<IReadOnlyCollection<object>, Task> Delete) : TypeConfiguration;
 
-public record TypeConfiguration();
+public abstract record TypeConfiguration();
 
 public record TypeConfigurationWithType<T>(TypeConfiguration<T> TypeConfiguration, Type Type);
 
-public class DataPlugin : MessageHubPlugin<DataPlugin, IWorkspace>,
-                          IMessageHandler<GetManyRequest<Scenario>>
+
+
+
+public class DataPlugin : MessageHubPlugin<DataPlugin, IWorkspace>
 {
     [Inject] private IActivityService activityService;
 
@@ -106,7 +127,8 @@ public class DataPlugin : MessageHubPlugin<DataPlugin, IWorkspace>,
     public DataPlugin(IMessageHub hub, MessageHubConfiguration configuration, 
                       Func<DataPluginConfiguration, DataPluginConfiguration> dataConfiguration) : base(hub)
     {
-        Register(HandleGetRequest);  // This takes care of all Read (CRUD)
+        Register(HandleGetRequest);              // This takes care of all Read (CRUD)
+        Register(HandleUpdateAndDeleteRequest);  // This takes care of all Update and Delete (CRUD)
 
         DataConfiguration = dataConfiguration(DataConfiguration);
     }
@@ -123,26 +145,59 @@ public class DataPlugin : MessageHubPlugin<DataPlugin, IWorkspace>,
         }
     }
 
-    public IMessageDelivery HandleMessage(IMessageDelivery<GetManyRequest<Scenario>> request)
+    private IMessageDelivery HandleUpdateAndDeleteRequest(IMessageDelivery request)
     {
-        var data = new Scenario[100];
-        Hub.Post(data, o => o.ResponseFor(request));
+        var type = request.Message.GetType();
+        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(UpdateBatchRequest<>) || type.GetGenericTypeDefinition() == typeof(DeleteBatchRequest<>))
+        {
+            var elementType = type.GetGenericArguments().First();
+            var typeConfig = DataConfiguration.TypeConfigurations.FirstOrDefault(x => 
+                    x.GetType().GetGenericArguments().First() == elementType );  // TODO: check whether this works
+
+            if (typeConfig is null) return request;
+            var config = (TypeConfiguration<object>)typeConfig;
+
+            if(type.GetGenericTypeDefinition() == typeof(UpdateBatchRequest<>))
+            {
+                var getElementsMethod = ReflectionHelper.GetMethodGeneric<DataPlugin>(x => x.UpdateElements<object>(null, null));
+                getElementsMethod.MakeGenericMethod(elementType).InvokeAsFunction(this, config.Save);
+            }
+            else if(type.GetGenericTypeDefinition() == typeof(DeleteBatchRequest<>))
+            {
+                var getElementsMethod = ReflectionHelper.GetMethodGeneric<DataPlugin>(x => x.DeleteElements<object>(null, null));
+                getElementsMethod.MakeGenericMethod(elementType).InvokeAsFunction(this, config.Delete);
+            }
+        }
         return request.Processed();
     }
 
-    private IMessageDelivery HandleGetRequest(IMessageDelivery request)  // Read request of the CRUD
+    async Task UpdateElements<T>(IMessageDelivery<UpdateBatchRequest<T>> request, Func<IReadOnlyCollection<T>, Task> save) where T : class
+    {
+        var items = request.Message.Elements;
+        await save(items);                     // save to db
+        await State.UpdateAsync(items);        // update the state in memory (workspace)
+        Hub.Post(new DataChanged(items));      // notify all subscribers that the data has changed
+    }
+
+    async Task DeleteElements<T>(IMessageDelivery<DeleteBatchRequest<T>> request, Func<IReadOnlyCollection<T>, Task> delete) where T : class
+    {
+        var items = request.Message.Elements;
+        await delete(items);
+        await State.DeleteAsync(items);
+        Hub.Post(new DataDeleted(items));
+    }
+
+    private IMessageDelivery HandleGetRequest(IMessageDelivery request)
     {
         var type = request.Message.GetType();
         if(type.IsGenericType && type.GetGenericTypeDefinition() == typeof(GetManyRequest<>))
         {
             var elementType = type.GetGenericArguments().First();
-            return (IMessageDelivery)GetElementsMethod.MakeGenericMethod(elementType).InvokeAsFunction(this, request);
+            var getElementsMethod = ReflectionHelper.GetMethodGeneric<DataPlugin>(x => x.GetElements<object>(null));
+            return (IMessageDelivery)getElementsMethod.MakeGenericMethod(elementType).InvokeAsFunction(this, request);
         }
         return request;
     }
-
-    private static readonly MethodInfo GetElementsMethod
-        = ReflectionHelper.GetMethodGeneric<DataPlugin>(x => x.GetElements<object>(null));
 
     private IMessageDelivery GetElements<T>(IMessageDelivery<GetManyRequest<T>> request) where T : class
     {
