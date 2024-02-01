200a5ff8b50829942e7a6f8f8982a9b40d1ff48f
amuolo <and.muolo@gmail.com>
2024-02-01 17:47:22 +0100

    data plugin to opensmc

---
+++ OpenSmc.Ifrs17/OpenSmc.Ifrs17.ReferenceDataHub/ConfigurationsIfrs.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OpenSmc.DataPlugin;
+using OpenSmc.DataSource.Abstractions;
+using OpenSmc.Ifrs17.Domain.DataModel;
+using OpenSmc.Ifrs17.Domain.DataModel.KeyedDimensions;
+using OpenSmc.Messaging;
+
+namespace OpenSmc.Ifrs17.ReferenceDataHub;
+
+/* AM (1.2.2024) todo list:
+ *  a) organize better the IFRS17 project structure
+ *  b) orchestrate which data go to which hub, e.g. refDataHub owns all dimensions
+ *      write the configuration for parameters, transactionalData, etc etc
+ *  c) finish setting up all model hubs simply by means of this generic DataPlugin
+ *  d) implement tests for the DataPlugin by adding tests in the OpenSMC repo
+ *  e) test the IFRS17 model hubs writing the level above this configurations
+ *      to do this check the existing tests in the OpenSMC, e.g. MessageHubTest
+ *      then write the configuration to define routing, addresses, forwarding, etc $
+ *  f) think at the viewModelHub, what to do here? where to start?
+ *      look at the existing tests in OpenSMC, e.g. LayoutTest
+ *  g) monitor the development of the import/export plugin so that we can use them here
+ *      in smc v1 
+ */
+public static class IfrsConfiguration
+{
+    public static MessageHubConfiguration ConfigurationReferenceDataHub(this MessageHubConfiguration configuration)
+    {
+        // TODO: this needs to be registered in the higher level
+        var dataSource = configuration.ServiceProvider.GetService<IDataSource>();
+
+        return configuration
+            .AddData(data => data
+                .WithType<Scenario>( // TODO: Extend With Type to avoid monkey code
+                    async () => await dataSource.Query<Scenario>().ToArrayAsync(),
+                    (scenarios) => dataSource.UpdateAsync(scenarios),
+                    (scenarios) => dataSource.DeleteAsync(scenarios))
+                .WithType<AmountType>(
+                    async () => await dataSource.Query<AmountType>().ToArrayAsync(),
+                    (amountTypes) => dataSource.UpdateAsync(amountTypes),
+                    (amountTypes) => dataSource.DeleteAsync(amountTypes)));
+    }
+
+    public static MessageHubConfiguration ConfigurationTransactionalDataHub(this MessageHubConfiguration configuration)
+    {
+        // TODO: this needs to be registered in the higher level
+        var dataSource = configuration.ServiceProvider.GetService<IDataSource>();
+
+        return configuration
+            .AddData(data => data.WithType<RawVariable>(
+                async () => await dataSource.Query<RawVariable>().ToArrayAsync(),
+                (scenarios) => dataSource.UpdateAsync(scenarios),
+                (scenarios) => dataSource.DeleteAsync(scenarios))); /* This is delete of data, not of the hub */
+        /* Delete of Hub must be implemented separately (pr)*/
+    }
+
+    public static MessageHubConfiguration ConfigurationViewModelHub(this MessageHubConfiguration configuration)
+    {
+        // TODO: this needs to be registered in the higher level
+        //var dataSource = configuration.ServiceProvider.GetService<IDataSource>();
+        // TODO: What content should be here? -A.K.
+
+        /* AM (1.2.2024) personal idea:
+         *  The view model (VM) hub is attached to the browser tab, namely there is one per tab.
+         *  When one control (e.g. import button) hosted in this VM signals that an import has to start,
+         *  an import request should be sent to the ImportPlugin. Hence the ImportPlugin has to be 
+         *  'attached' to this VM. 
+         *  
+         *  This also grants scalability. Because ImportPlugin knows the import formats and calls 
+         *  the method to perform the calculation, this setup ensures one calculation can take place
+         *  per browser tab.
+         */
+
+        return configuration;
+    }
+}
+
+
+
+/* Outdated comment
+ * 
+ * Create request:
+ *      RegisterCallback --> send ImportRequest to ImportHub
+ *      
+ *          ImportHub: processes this request and respond with data
+ *  
+ *      upon receiving the response, this hub has the data and can initialize the state
+ *  
+ * Read requests:
+ *      send a response with the data needed
+ *      
+ * Update requests:
+ *      perform validation
+ *      
+ *      if validation fails, respond with the failing log
+ *      
+ *      if validation succeeds, respond with DataChangedEvent 
+ *      
+ *      DataChangedEvent will be received to all subscribers, e.g.
+ *       - ui controls that need to update with new data
+ *       - DataHub receives this and updates the ref data in its cache
+ */
+
