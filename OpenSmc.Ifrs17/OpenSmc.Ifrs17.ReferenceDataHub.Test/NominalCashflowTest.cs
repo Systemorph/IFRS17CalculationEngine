87f5764528f483301c092053aa306bd05eac6d90
Andrey Katz <akatz@systemorph.com>
2024-02-16 16:03:43 +0100

    new empty test file

---
+++ OpenSmc.Ifrs17/OpenSmc.Ifrs17.ReferenceDataHub.Test/NominalCashflowTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSmc.Ifrs17.ReferenceDataHub.Test
+{
+    internal class NominalCashflowTest
+    {
+
+    }
+}
