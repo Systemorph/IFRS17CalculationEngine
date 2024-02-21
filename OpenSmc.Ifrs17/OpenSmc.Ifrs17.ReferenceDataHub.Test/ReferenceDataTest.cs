394e9f54ba6dda5b1ec2d75a436bb0ec62bf4f5f
Dmitry Kalabin <dkalabin@systemorph.com>
2024-02-21 23:38:59 +0100

    PoC with new flexible ConfigureCategory() extension

--- OpenSmc.Ifrs17/OpenSmc.Ifrs17.ReferenceDataHub.Test/ReferenceDataTest.cs
+++ OpenSmc.Ifrs17/OpenSmc.Ifrs17.ReferenceDataHub.Test/ReferenceDataTest.cs
@@ -1,107 +1,153 @@
+using System.Reflection;
 using FluentAssertions;
+using FluentAssertions.Execution;
 using OpenSmc.Activities;
 using OpenSmc.Data;
 using OpenSmc.Hub.Fixture;
-using OpenSmc.Import;
-using OpenSmc.Messaging;
-using Xunit;
-using Xunit.Abstractions;
 using OpenSmc.Ifrs17.DataTypes.DataModel;
 using OpenSmc.Ifrs17.DataTypes.DataModel.FinancialDataDimensions;
 using OpenSmc.Ifrs17.DataTypes.DataModel.KeyedDimensions;
 using OpenSmc.Ifrs17.DataTypes.Constants;
+using OpenSmc.Import;
+using OpenSmc.Messaging;
+using OpenSmc.Reflection;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace OpenSmc.Ifrs17.ReferenceDataHub.Test;
 
 public class ImportReferenceDataTest(ITestOutputHelper output) : HubTestBase(output)
+{
+    public static readonly Dictionary<Type, IEnumerable<object>> ReferenceDataDomain
+        =
+        new()
+        {
+            { typeof(AmountType), Array.Empty<AmountType>() },
+            { typeof(DeferrableAmountType), new DeferrableAmountType[] {} },
+            { typeof(AocType), new AocType[] {} },
+            { typeof(StructureType), new StructureType[] {} },
+            { typeof(CreditRiskRating), new CreditRiskRating[] {} },
+            { typeof(Currency), new Currency[] {} },
+            { typeof(EconomicBasis), new EconomicBasis[] {} },
+            { typeof(EstimateType), new EstimateType[] {} },
+            { typeof(LiabilityType), new LiabilityType[] {} },
+            { typeof(LineOfBusiness), new LineOfBusiness[] {} },
+            { typeof(Novelty), new  Novelty[] {} },
+            { typeof(OciType), new  OciType[] {} },
+            { typeof(Partner), new  Partner[] {} },
+            { typeof(BsVariableType), new  BsVariableType[] {} },
+            { typeof(PnlVariableType), new  PnlVariableType[] {} },
+            { typeof(RiskDriver), new  RiskDriver[] {} },
+            { typeof(Scenario), new  Scenario[] {} },
+            { typeof(ValuationApproach), new  ValuationApproach[] {} },
+            { typeof(ProjectionConfiguration), new  ProjectionConfiguration[] {} },
+        };
+
+    private static readonly IEnumerable<TypeDomainDescriptor> ReferenceDataDomainExtra =
+        new TypeDomainDescriptor[]
+        {
+            new TypeDomainDescriptor<AocConfiguration>() { TypeConfig = t => t.WithKey(x => (x.Year, x.Month, x.AocType, x.Novelty)) },
+        };
+
+    protected override MessageHubConfiguration ConfigureHost(MessageHubConfiguration configuration)
     {
-        public static readonly Dictionary<Type, IEnumerable<object>> ReferenceDataDomain
-            =
-            new()
-            {
-                { typeof(AmountType), Array.Empty<AmountType>() },
-                { typeof(DeferrableAmountType), new DeferrableAmountType[] {} },
-                { typeof(AocType), new AocType[] {} },
-                { typeof(StructureType), new StructureType[] {} },
-                { typeof(CreditRiskRating), new CreditRiskRating[] {} },
-                { typeof(Currency), new Currency[] {} },
-                { typeof(EconomicBasis), new EconomicBasis[] {} },
-                { typeof(EstimateType), new EstimateType[] {} },
-                { typeof(LiabilityType), new LiabilityType[] {} },
-                { typeof(LineOfBusiness), new LineOfBusiness[] {} },
-                { typeof(Novelty), new  Novelty[] {} },
-                { typeof(OciType), new  OciType[] {} },
-                { typeof(Partner), new  Partner[] {} },
-                { typeof(BsVariableType), new  BsVariableType[] {} },
-                { typeof(PnlVariableType), new  PnlVariableType[] {} },
-                { typeof(RiskDriver), new  RiskDriver[] {} },
-                { typeof(Scenario), new  Scenario[] {} },
-                { typeof(ValuationApproach), new  ValuationApproach[] {} },
-                { typeof(ProjectionConfiguration), new  ProjectionConfiguration[] {} },
-            };
+        return base.ConfigureHost(configuration)
+            .AddData(data => data.WithDataSource(nameof(DataSource), 
+                    source => source.ConfigureCategory(ReferenceDataDomain)
+                        .ConfigureCategory(ReferenceDataDomainExtra)
+                    ))
+            .AddImport(import => import);
+    }
 
-        protected override MessageHubConfiguration ConfigureHost(MessageHubConfiguration configuration)
+    [Fact]
+    public async Task ImportDimensionTest()
+    {
+        var client = GetClient();
+        var importRequest = new ImportRequest(TemplateDimensions.Csv);
+        var importResponse = await client.AwaitResponse(importRequest, o => o.WithTarget(new HostAddress()));
+        importResponse.Message.Log.Status.Should().Be(ActivityLogStatus.Succeeded);
+
+        var atItems = await client.AwaitResponse(new GetManyRequest<AmountType>(),
+            o => o.WithTarget(new HostAddress()));
+        var datItems = await client.AwaitResponse(new GetManyRequest<DeferrableAmountType>(),
+            o => o.WithTarget(new HostAddress()));
+        var aocItems = await client.AwaitResponse(new GetManyRequest<AocType>(),
+            o => o.WithTarget(new HostAddress()));
+        var aoccItems = await client.AwaitResponse(new GetManyRequest<AocConfiguration>(),
+            o => o.WithTarget(new HostAddress()));
+        var stItems = await client.AwaitResponse(new GetManyRequest<StructureType>(),
+            o => o.WithTarget(new HostAddress()));
+        var crrItems = await client.AwaitResponse(new GetManyRequest<CreditRiskRating>(),
+            o => o.WithTarget(new HostAddress()));
+        var cItems = await client.AwaitResponse(new GetManyRequest<Currency>(),
+            o => o.WithTarget(new HostAddress()));
+        var ecItems = await client.AwaitResponse(new GetManyRequest<EconomicBasis>(),
+            o => o.WithTarget(new HostAddress()));
+        var esItems = await client.AwaitResponse(new GetManyRequest<EstimateType>(),
+            o => o.WithTarget(new HostAddress()));
+        var ltItems = await client.AwaitResponse(new GetManyRequest<LiabilityType>(),
+            o => o.WithTarget(new HostAddress()));
+        var lobItems = await client.AwaitResponse(new GetManyRequest<LineOfBusiness>(),
+            o => o.WithTarget(new HostAddress()));
+        var nItems = await client.AwaitResponse(new GetManyRequest<Novelty>(),
+            o => o.WithTarget(new HostAddress()));
+        var otItems = await client.AwaitResponse(new GetManyRequest<OciType>(),
+            o => o.WithTarget(new HostAddress()));
+        var pItems = await client.AwaitResponse(new GetManyRequest<Partner>(),
+            o => o.WithTarget(new HostAddress()));
+        var bvtItems = await client.AwaitResponse(new GetManyRequest<BsVariableType>(),
+            o => o.WithTarget(new HostAddress()));
+        var pvt = await client.AwaitResponse(new GetManyRequest<PnlVariableType>(),
+            o => o.WithTarget(new HostAddress()));
+        var rdItems = await client.AwaitResponse(new GetManyRequest<RiskDriver>(),
+            o => o.WithTarget(new HostAddress()));
+        var sItems = await client.AwaitResponse(new GetManyRequest<Scenario>(),
+            o => o.WithTarget(new HostAddress()));
+        var vaItems = await client.AwaitResponse(new GetManyRequest<ValuationApproach>(),
+            o => o.WithTarget(new HostAddress()));
+        var pcItems = await client.AwaitResponse(new GetManyRequest<ProjectionConfiguration>(),
+            o => o.WithTarget(new HostAddress()));
+
+        using (new AssertionScope())
         {
-            return base.ConfigureHost(configuration)
-                .AddData(data => data.WithDataSource(nameof(DataSource), 
-                        source => source.ConfigureCategory(ReferenceDataDomain)
-                            .WithType<AocConfiguration>(t => t.WithKey(x => (x.Year, x.Month, x.AocType, x.Novelty)).WithInitialData(Array.Empty<AocConfiguration>()))
-                        ))
-                .AddImport(import => import);
-        }
-
-        [Fact]
-        public async Task ImportDimensionTest()
-        {
-            var client = GetClient();
-            var importRequest = new ImportRequest(TemplateDimensions.Csv);
-            var importResponse = await client.AwaitResponse(importRequest, o => o.WithTarget(new HostAddress()));
-            importResponse.Message.Log.Status.Should().Be(ActivityLogStatus.Succeeded);
-
-            var atItems = await client.AwaitResponse(new GetManyRequest<AmountType>(),
-                o => o.WithTarget(new HostAddress()));
-            var datItems = await client.AwaitResponse(new GetManyRequest<DeferrableAmountType>(),
-                o => o.WithTarget(new HostAddress()));
-            var aocItems = await client.AwaitResponse(new GetManyRequest<AocType>(),
-                o => o.WithTarget(new HostAddress()));
-            var aoccItems = await client.AwaitResponse(new GetManyRequest<AocConfiguration>(),
-                o => o.WithTarget(new HostAddress()));
-            var stItems = await client.AwaitResponse(new GetManyRequest<StructureType>(),
-                o => o.WithTarget(new HostAddress()));
-            var crrItems = await client.AwaitResponse(new GetManyRequest<CreditRiskRating>(),
-                o => o.WithTarget(new HostAddress()));
-            var cItems = await client.AwaitResponse(new GetManyRequest<Currency>(),
-                o => o.WithTarget(new HostAddress()));
-            var ecItems = await client.AwaitResponse(new GetManyRequest<EconomicBasis>(),
-                o => o.WithTarget(new HostAddress()));
-            var esItems = await client.AwaitResponse(new GetManyRequest<EstimateType>(),
-                o => o.WithTarget(new HostAddress()));
-            var ltItems = await client.AwaitResponse(new GetManyRequest<LiabilityType>(),
-                o => o.WithTarget(new HostAddress()));
-            var lobItems = await client.AwaitResponse(new GetManyRequest<LineOfBusiness>(),
-                o => o.WithTarget(new HostAddress()));
-            var nItems = await client.AwaitResponse(new GetManyRequest<Novelty>(),
-                o => o.WithTarget(new HostAddress()));
-            var otItems = await client.AwaitResponse(new GetManyRequest<OciType>(),
-                o => o.WithTarget(new HostAddress()));
-            var pItemsn = await client.AwaitResponse(new GetManyRequest<Partner>(),
-                o => o.WithTarget(new HostAddress()));
-            var bvtItems = await client.AwaitResponse(new GetManyRequest<BsVariableType>(),
-                o => o.WithTarget(new HostAddress()));
-            var pvt = await client.AwaitResponse(new GetManyRequest<PnlVariableType>(),
-                o => o.WithTarget(new HostAddress()));
-            var rdItems = await client.AwaitResponse(new GetManyRequest<RiskDriver>(),
-                o => o.WithTarget(new HostAddress()));
-            var sItems = await client.AwaitResponse(new GetManyRequest<Scenario>(),
-                o => o.WithTarget(new HostAddress()));
-            var vaItems = await client.AwaitResponse(new GetManyRequest<ValuationApproach>(),
-                o => o.WithTarget(new HostAddress()));
-            var pcItems = await client.AwaitResponse(new GetManyRequest<ProjectionConfiguration>(),
-                o => o.WithTarget(new HostAddress()));
-
             atItems.Message.Items.Should().HaveCount(17);
             datItems.Message.Items.Should().HaveCount(2);
             aocItems.Message.Items.Should().HaveCount(17);
             aoccItems.Message.Items.Should().HaveCount(21);
+            stItems.Message.Items.Should().HaveCount(8);
+            crrItems.Message.Items.Should().HaveCount(22);
+            cItems.Message.Items.Should().HaveCount(10);
+            ecItems.Message.Items.Should().HaveCount(3);
+            esItems.Message.Items.Should().HaveCount(15);
+            ltItems.Message.Items.Should().HaveCount(2);
+            lobItems.Message.Items.Should().HaveCount(15);
+            nItems.Message.Items.Should().HaveCount(3);
+            otItems.Message.Items.Should().HaveCount(1);
+            pItems.Message.Items.Should().HaveCount(2);
+            bvtItems.Message.Items.Should().HaveCount(1);
+            pvt.Message.Items.Should().HaveCount(50);
+            rdItems.Message.Items.Should().HaveCount(1);
+            sItems.Message.Items.Should().HaveCount(16);
+            vaItems.Message.Items.Should().HaveCount(2);
+            pcItems.Message.Items.Should().HaveCount(20);
         }
+    }
+}
+
+public record TypeDomainDescriptor;
+public record TypeDomainDescriptor<T>() : TypeDomainDescriptor where T : class
+{
+    public IEnumerable<T> InitialData { get; init; } = Array.Empty<T>();
+    public Func<TypeSource<T>, TypeSource<T>> TypeConfig { get; init; } = typeConfig => typeConfig;
+}
+
+public static class DataSourceDomainExtensions
+{
+    public static DataSource ConfigureCategory(this DataSource dataSource, IEnumerable<TypeDomainDescriptor> typeDescriptors)
+        => typeDescriptors.Aggregate(dataSource, (ds, t) => (DataSource)ConfigureTypeMethod.MakeGenericMethod(t.GetType().GetGenericArguments().First()).InvokeAsFunction(ds, t));
+
+    private static readonly MethodInfo ConfigureTypeMethod = ReflectionHelper.GetStaticMethodGeneric(() => ConfigureType<object>(null, null));
+
+    private static DataSource ConfigureType<T>(DataSource dataSource, TypeDomainDescriptor<T> typeDescriptor) where T : class
+        => dataSource.WithType<T>(t => typeDescriptor.TypeConfig(t.WithInitialData(typeDescriptor.InitialData)));
 }
