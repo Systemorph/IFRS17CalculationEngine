45334e8d7d5270b4884598aa37a03a1c3fc600a1
Andrey Katz <akatz@systemorph.com>
2024-02-26 14:00:43 +0100

    digitalized simple value

---
+++ OpenSmc.Ifrs17/OpenSmc.Ifrs17.IfrsVariableHub/IfrsVariableHubConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using OpenSmc.Activities;
+using OpenSmc.Messaging;
+using OpenSmc.ServiceProvider;
+
+namespace OpenSmc.Ifrs17.IfrsVariableHub;
+
+/* For now, this hub owns all data except Dimensions and ReportVariable (subject to change).
+ */
+public static class IfrsVariableHubConfiguration
+{
+    public static MessageHubConfiguration ConfigureIfrsDataDictInit(this MessageHubConfiguration configuration) =>
+        configuration;
+
+}
+
+
+
