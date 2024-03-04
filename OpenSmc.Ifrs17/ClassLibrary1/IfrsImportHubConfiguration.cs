c927662268b754d491ab4541f890449d19b754a4
Andrey Katz <akatz@systemorph.com>
2024-03-04 15:36:12 +0100

    start with basic configs

--- OpenSmc.Ifrs17/ClassLibrary1/IfrsImportHubConfiguration.cs
+++ OpenSmc.Ifrs17/ClassLibrary1/IfrsImportHubConfiguration.cs
@@ -1,7 +1,15 @@
-﻿namespace OpenSmc.Ifrs17.ImportHubs
+﻿using OpenSmc.Ifrs17.DataNodeHub;
+using OpenSmc.Messaging;
+
+namespace OpenSmc.Ifrs17.ImportHubs
 {
     public static class IfrsImportHubConfiguration
     {
+        public static MessageHubConfiguration ConfigureSimpleDataImportHub(MessageHubConfiguration configuration)
+        {
+            var refDataNodeAddress = new DataNodeAddress(configuration.Address);
+            return configuration.WithHostedHub(refDataNodeAddress, config => config);
+        }
 
     }
 }
