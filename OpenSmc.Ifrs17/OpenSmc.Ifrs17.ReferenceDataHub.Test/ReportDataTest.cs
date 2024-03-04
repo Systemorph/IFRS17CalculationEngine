e2d995ff9a97981ae4149fa950570d7d3d3f6995
amuolo <and.muolo@gmail.com>
2024-03-04 10:07:37 +0100

    draft report data hub

---
+++ OpenSmc.Ifrs17/OpenSmc.Ifrs17.ReferenceDataHub.Test/ReportDataTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSmc.Ifrs17.Hub.Test;
+
+internal class ReportDataTest
+{
+}
--- OpenSmc.Ifrs17/ReportHub/ReportHubConfiguration.cs
+++ OpenSmc.Ifrs17/ReportHub/ReportHubConfiguration.cs
@@ -5,48 +5,50 @@ using OpenSmc.Ifrs17.DataNodeHub;
 using OpenSmc.Ifrs17.ReferenceDataHub;
 using OpenSmc.Ifrs17.ParameterDataHub;
 using OpenSmc.Ifrs17.IfrsVariableHub;
+using OpenSmc.Ifrs17.DataTypes.DataModel.FinancialDataDimensions;
+using OpenSmc.Ifrs17.DataTypes.DataModel;
+using OpenSmc.Ifrs17.Utils;
+using OpenSmc.Data.Persistence;
+using OpenSmc.Scopes.Proxy;
+using Microsoft.Extensions.DependencyInjection;
+using OpenSmc.Ifrs17.DataTypes.DataModel.TransactionalData;
 
 namespace OpenSmc.Ifrs17.ReportHub;
 
 public static class ReportHubConfiguration
 {
+    public static MessageHubConfiguration ConfigureReportDataHub(this MessageHubConfiguration configuration)
+    {
+        var refDataAddress = new ReferenceDataAddress(configuration.Address);
+        var dataNodeAddress = new DataNodeAddress(configuration.Address);
+        var parameterAddress = new ParameterAddress(configuration.Address);
+        var ifrsVarAddress = new IfrsVariableAddress(configuration.Address);
+        var reportAddress = new ReportAddress(configuration.Address);
 
+        return configuration
+            .WithServices(services => services.AddSingleton<ScopeFactory>())
+            
+            .WithHostedHub(refDataAddress, config => config.ConfigureReferenceDataDictInit())
+            .WithHostedHub(dataNodeAddress, config => config.ConfigureDataNodeDataDictInit())
+            .WithHostedHub(parameterAddress, config => config.ConfigureParameterDataDictInit())
+            .WithHostedHub(ifrsVarAddress, config => config.ConfigureIfrsDataDictInit(2020, 12, "CH", default)) // TODO: ???
+            
+            .WithHostedHub(reportAddress, config => config
+                .AddData(data => data
+                    .FromHub(refDataAddress, ds => ds
+                        .WithType<AmountType>().WithType<LineOfBusiness>())
+                    .FromHub(ifrsVarAddress, ds => ds
+                        .WithType<IfrsVariable>())
+                    .AddCustomInitialization(ReportInit(config))));
+    }
 
-    //public static MessageHubConfiguration ConfigureReportHubSimple(this MessageHubConfiguration configuration)
-    //{
-    //    var refDataAddress = new ReferenceDataAddress(configuration.Address);
-    //    var dataNodeAddress = new DataNodeAddress(configuration.Address);
-    //    var reportAddress = new ReportAddress(configuration.Address);
-
-    //    return configuration
-    //        .WithHostedHub(refDataAddress, config => config
-    //            .AddImport(import => import)
-    //            .AddData(dc => dc
-    //            .WithDataSource("ReportDataSource",
-    //                ds => ds.ConfigureCategory(ReferenceDataHubConfiguration.ReferenceDataDomain)
-    //                        .ConfigureCategory(ReferenceDataHubConfiguration.ReferenceDataDomainExtra)
-    //                        .ConfigureCategory(ParameterHubConfiguration.ParametersDomain)
-    //                        .ConfigureCategory(ParameterHubConfiguration.ParametersDomainExtra)
-    //                        .ConfigureCategory(DataNodeHubConfiguration.DataNodeDomain)
-    //                        .ConfigureCategory(DataNodeHubConfiguration.DataNodeDomainExtra)
-    //                        //.ConfigureCategory(IfrsVariablesHubConfiguration.DataNodeDomain)
-    //                        //.ConfigureCategory(IfrsVariablesHubConfiguration.DataNodeDomainExtra)
-    //                        )
-    //            .WithInitialization(ReportInit(config))));
-    //}
-
-    //public static Func<IMessageHub, CombinedWorkspaceState, CancellationToken, Task> ReportInit(MessageHubConfiguration config)
-    //{
-    //    return async (hub, workspace, cancellationToken) =>
-    //    {
-    //        await ReferenceDataHubConfiguration.RefDataInit(config, TemplateDimensions.Csv).Invoke(hub, workspace, cancellationToken);
-    //        await ParameterHubConfiguration.ParametersInit(config, TemplateParameter.Csv).Invoke(hub, workspace, cancellationToken);
-    //        await DataNodeHubConfiguration.DataNodeInit(config, "", null).Invoke(hub, workspace, cancellationToken);
-    //        await IfrsVariablesHubConfiguration.IfrsVarsInit(config, "").Invoke(hub, workspace, cancellationToken);
-
-    //        // TODO: WIP
-    //        var storage = new ReportStorage(workspace);
-    //        storage.Initialize((2020, 12), "CH", null, DataTypes.Constants.Enumerates.CurrencyType.Group);
-    //    };
-    //}
+    public static Action<HubDataSource, ScopeFactory> ReportInit(MessageHubConfiguration config)
+    {
+        return (workspace, scopeFactory) =>
+        {
+            // TODO: WIP
+            var storage = new ReportStorage(workspace);
+            storage.Initialize((2020, 12), "CH", null, DataTypes.Constants.Enumerates.CurrencyType.Group);
+        };
+    }
 }
