c5f0cfc753bdfb3899fdc7e856653c17b0864ead
amuolo <and.muolo@gmail.com>
2024-03-04 13:05:48 +0100

    wip

--- OpenSmc.Ifrs17/OpenSmc.Ifrs17.ReferenceDataHub.Test/ReportDataTest.cs
+++ OpenSmc.Ifrs17/OpenSmc.Ifrs17.ReferenceDataHub.Test/ReportDataTest.cs
@@ -1,11 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenSmc.Data;
+using OpenSmc.Hub.Fixture;
+using OpenSmc.Ifrs17.DataTypes.DataModel;
+using OpenSmc.Ifrs17.ReportHub;
+using OpenSmc.Messaging;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace OpenSmc.Ifrs17.Hub.Test;
 
-internal class ReportDataTest
+public class ReportDataTest(ITestOutputHelper output) : HubTestBase(output)
 {
+    protected override MessageHubConfiguration ConfigureHost(MessageHubConfiguration configuration)
+    {
+        return base.ConfigureHost(configuration).ConfigureReportDataHub();
+    }
+
+    [Fact]
+    public async Task InitParameterDataTest()
+    {
+        var client = GetClient();
+
+        var exchangeRateData = await client.AwaitResponse(new GetManyRequest<ExchangeRate>(), o => o.WithTarget(new HostAddress()));
+        var cdrData = await client.AwaitResponse(new GetManyRequest<CreditDefaultRate>(), o => o.WithTarget(new HostAddress()));
+        var partnerRatingData = await client.AwaitResponse(new GetManyRequest<PartnerRating>(), o => o.WithTarget(new HostAddress()));
+
+        exchangeRateData.Message.Items.Should().HaveCount(12);
+        cdrData.Message.Items.Should().HaveCount(22);
+        partnerRatingData.Message.Items.Should().HaveCount(3);
+    }
 }
--- OpenSmc.Ifrs17/ReportHub/ReportHubConfiguration.cs
+++ OpenSmc.Ifrs17/ReportHub/ReportHubConfiguration.cs
@@ -1,17 +1,16 @@
 ﻿using OpenSmc.Data;
 using OpenSmc.Messaging;
-using OpenSmc.Import;
 using OpenSmc.Ifrs17.DataNodeHub;
 using OpenSmc.Ifrs17.ReferenceDataHub;
 using OpenSmc.Ifrs17.ParameterDataHub;
 using OpenSmc.Ifrs17.IfrsVariableHub;
 using OpenSmc.Ifrs17.DataTypes.DataModel.FinancialDataDimensions;
-using OpenSmc.Ifrs17.DataTypes.DataModel;
-using OpenSmc.Ifrs17.Utils;
 using OpenSmc.Data.Persistence;
 using OpenSmc.Scopes.Proxy;
 using Microsoft.Extensions.DependencyInjection;
 using OpenSmc.Ifrs17.DataTypes.DataModel.TransactionalData;
+using OpenSmc.Ifrs17.DataTypes.DataModel;
+using OpenSmc.Ifrs17.DataTypes.DataModel.KeyedDimensions;
 
 namespace OpenSmc.Ifrs17.ReportHub;
 
@@ -35,8 +34,16 @@ public static class ReportHubConfiguration
             
             .WithHostedHub(reportAddress, config => config
                 .AddData(data => data
+                    .FromConfigurableDataSource("ReportDataSource", ds => ds
+                        .WithType<ReportVariable>(t => t.WithKey(ReportVariableKey)))
                     .FromHub(refDataAddress, ds => ds
+                        // TODO: complete this list
                         .WithType<AmountType>().WithType<LineOfBusiness>())
+                    .FromHub(dataNodeAddress, ds => ds
+                        .WithType<InsurancePortfolio>().WithType<GroupOfInsuranceContract>()
+                        .WithType<ReinsurancePortfolio>().WithType<GroupOfReinsuranceContract>())
+                    .FromHub(parameterAddress, ds => ds 
+                        .WithType<ExchangeRate>().WithType<CreditDefaultRate>().WithType<PartnerRating>())
                     .FromHub(ifrsVarAddress, ds => ds
                         .WithType<IfrsVariable>())
                     .AddCustomInitialization(ReportInit(config))));
@@ -46,9 +53,22 @@ public static class ReportHubConfiguration
     {
         return (workspace, scopeFactory) =>
         {
+            //workspace.GetData<IfrsVariable>();
+            
             // TODO: WIP
             var storage = new ReportStorage(workspace);
             storage.Initialize((2020, 12), "CH", null, DataTypes.Constants.Enumerates.CurrencyType.Group);
+
+            // TODO: instantiate a new scope registry here
+            //scopeFactory.ForSingleton<>();
+
+            // TODO
         };
     }
+
+    private static Func<ReportVariable, object> ReportVariableKey => x =>
+        (x.ReportingNode, x.Scenario, x.Currency, x.Novelty, x.FunctionalCurrency, x.ContractualCurrency, x.GroupOfContract,
+         x.Portfolio, x.LineOfBusiness, x.LiabilityType, x.InitialProfitability, x.ValuationApproach, x.AnnualCohort,
+         x.OciType, x.Partner, x.IsReinsurance, x.AccidentYear, x.ServicePeriod, x.Projection, x.VariableType, x.Novelty,
+         x.AmountType, x.EstimateType, x.EconomicBasis);
 }
